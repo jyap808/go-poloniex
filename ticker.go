c79b7ddc834be248d0b123973e8a486c9a4852e4
RedDec <dev@baryshnikov.net>
2017-11-14 17:06:30 +0800

    change ticker types from float to decimal

--- ticker.go
+++ ticker.go
@@ -1,17 +1,19 @@
 package poloniex
 
+import "github.com/shopspring/decimal"
+
 type Tickers struct {
 	Pair map[string]Ticker
 }
 
 type Ticker struct {
-	Last          float64 `json:"last,string"`
-	LowestAsk     float64 `json:"lowestAsk,string"`
-	HighestBid    float64 `json:"highestBid,string"`
-	PercentChange float64 `json:"percentChange,string"`
-	BaseVolume    float64 `json:"baseVolume,string"`
-	QuoteVolume   float64 `json:"quoteVolume,string"`
-	IsFrozen      int     `json:"isFrozen,string"`
-	High24Hr      float64 `json:"high24hr,string"`
-	Low24Hr       float64 `json:"low24hr,string"`
+	Last          decimal.Decimal `json:"last,string"`
+	LowestAsk     decimal.Decimal `json:"lowestAsk,string"`
+	HighestBid    decimal.Decimal `json:"highestBid,string"`
+	PercentChange decimal.Decimal `json:"percentChange,string"`
+	BaseVolume    decimal.Decimal `json:"baseVolume,string"`
+	QuoteVolume   decimal.Decimal `json:"quoteVolume,string"`
+	IsFrozen      int             `json:"isFrozen,string"`
+	High24Hr      decimal.Decimal `json:"high24hr,string"`
+	Low24Hr       decimal.Decimal `json:"low24hr,string"`
 }
