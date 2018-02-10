0dc6bbe12cfa1022ffe934ad3262516fe65e5b4f
Julian Yap <julianokyap@gmail.com>
2018-02-10 01:38:14 -1000

    Revert use of github.com/shopspring/decimal package

--- ticker.go
+++ ticker.go
@@ -1,20 +1,18 @@
 package poloniex
 
-import "github.com/shopspring/decimal"
-
 type Tickers struct {
 	Pair map[string]Ticker
 }
 
 type Ticker struct {
-	Id            int             `json:"id"`
-	Last          decimal.Decimal `json:"last,string"`
-	LowestAsk     decimal.Decimal `json:"lowestAsk,string"`
-	HighestBid    decimal.Decimal `json:"highestBid,string"`
-	PercentChange decimal.Decimal `json:"percentChange,string"`
-	BaseVolume    decimal.Decimal `json:"baseVolume,string"`
-	QuoteVolume   decimal.Decimal `json:"quoteVolume,string"`
-	IsFrozen      int             `json:"isFrozen,string"`
-	High24Hr      decimal.Decimal `json:"high24hr,string"`
-	Low24Hr       decimal.Decimal `json:"low24hr,string"`
+	Id            int     `json:"id"`
+	Last          float64 `json:"last,string"`
+	LowestAsk     float64 `json:"lowestAsk,string"`
+	HighestBid    float64 `json:"highestBid,string"`
+	PercentChange float64 `json:"percentChange,string"`
+	BaseVolume    float64 `json:"baseVolume,string"`
+	QuoteVolume   float64 `json:"quoteVolume,string"`
+	IsFrozen      int     `json:"isFrozen,string"`
+	High24Hr      float64 `json:"high24hr,string"`
+	Low24Hr       float64 `json:"low24hr,string"`
 }
